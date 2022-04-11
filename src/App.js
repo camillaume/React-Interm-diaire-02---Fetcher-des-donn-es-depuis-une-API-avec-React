47760898164196f548278f9ad938f2275842d66e
camillaume <camille-arvieu_student2022@wilder.school>
2022-04-11 17:00:26 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import QuoteCard from './components/QuoteCard';
+import axios from 'axios';
 
 function App() {
+  const sampleQuote = {
+    quote:
+      'By chilling my loins I increase the chances of impregnating my wife.',
+    character: 'Apu Nahasapeemapetilon',
+    image:
+      'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629',
+    characterDirection: 'Left',
+  };
+
+  const [quote, setQuote] = React.useState(sampleQuote);
+
+  const getQuote = () => {
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      .then((response) => response.data)
+      .then((data) => {
+        setQuote(data[0]);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <QuoteCard {...quote} />
+      <button type='button' onClick={getQuote}>
+        Get a Simpson Quote
+      </button>
+    </>
   );
 }
 
